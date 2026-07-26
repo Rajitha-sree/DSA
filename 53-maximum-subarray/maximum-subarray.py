33d651d9ac513683f2c7227d8e3a684f0ecd3b3f
Rajitha-sree <25071a12e2@vnrvjiet.in>
2026-07-26 10:34:44 +0530

    Time: 83 ms (60.92%) | Memory: 22 MB (11.13%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -4,13 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        c_sum = nums[0]
-        b_sum = nums[0]
+        c_sum = 0
+        b_sum = 0
 
         if (all(x<0 for x in nums)):
             return max(nums)
         else:
-            for i in nums[1:]:
+            for i in nums:
                 c_sum = max(i,c_sum+i)
                 b_sum = max(c_sum,b_sum)
 
