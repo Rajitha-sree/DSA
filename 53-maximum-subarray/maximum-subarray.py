4c0b795fd264f10ec32f7faa8e1235f13ccd5d0e
Rajitha-sree <25071a12e2@vnrvjiet.in>
2026-07-31 22:21:11 +0530

    Time: 50 ms (84.46%) | Memory: 22 MB (10.87%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -4,22 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # hm = {0:-1}
-        # max_s = -2147483648
-        # pref_sum = 0
-        # prev_sum = 0
-        # for i in range(len(nums)):
-        #     pref_sum += nums[i]
-        #     max_s = max(pref_sum,max_s)
-        #     if pref_sum < 0:
-        #         pref_sum = 0
-        #     # if pref_sum in hm:
-        #     #     hm[pref_sum] += 1
-
-        # return max_s
 
         pref_sum = 0
-        max_sum = -47524
+        max_sum = nums[0]
         for i in nums:
             pref_sum += i
             max_sum = max(max_sum,pref_sum)
