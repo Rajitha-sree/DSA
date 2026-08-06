5762301aeed5b9cc739f6e4438f276270ec741f5
BODDU RAJITHA SREE <25071a12e2@vnrvjiet.in>
2026-08-06 23:25:47 +0530

    Time: 15 ms (42.24%) | Memory: 14.2 MB (72.23%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -4,8 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        a = []
-        for i in nums:
-            a.append(i*i)
+        a = [0]*len(nums)
+        l = 0
+        r = len(nums)-1
+        k = len(nums)-1
+        while(k>=0):
+            if abs(nums[l])<abs(nums[r]):
+                a[k] = nums[r]*nums[r]
+                r-=1
+                k-=1
+            else:
+                a[k] = nums[l]*nums[l]
+                l+=1
+                k-=1
 
-        return sorted(a)
+
+        return a
