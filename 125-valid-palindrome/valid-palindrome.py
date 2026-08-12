c0bbf792959f5cef1fa9ff02259f5e898b15be45
BODDU RAJITHA SREE <25071a12e2@vnrvjiet.in>
2026-08-12 22:15:25 +0530

    Time: 16 ms (58.29%) | Memory: 12.7 MB (75.70%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        l = 0
+        r = len(s)-1
+        flag = 0
+        while l<r:
+            if not(s[l].isalnum()):
+                l+=1
+            elif not(s[r].isalnum()):
+                r-=1
+            else:
+                if s[l].lower() != s[r].lower():
+                    return False
+                else:
+                    l+=1
+                    r-=1
+
+        return True
+
