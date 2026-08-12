73c4f3c6b2b063a93a6622025458790b32a19922
BODDU RAJITHA SREE <25071a12e2@vnrvjiet.in>
2026-08-12 23:07:07 +0530

    Time: 74 ms (22.15%) | Memory: 16.4 MB (88.42%) - LeetSync

---
+++ 917-boats-to-save-people/boats-to-save-people.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def numRescueBoats(self, people, limit):
+        """
+        :type people: List[int]
+        :type limit: int
+        :rtype: int
+        """
+        n = len(people)
+        l = 0 
+        r = n-1
+        count = 0
+        people.sort()
+        while l<=r:
+            if people[l]+people[r] <= limit:
+                count+=1
+                l+=1
+                r-=1
+            else:
+                count+=1
+                r-=1
+
+        return count
+        
