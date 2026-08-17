c216084feaaa52343f1abac10561ea8cbd5029ed
BODDU RAJITHA SREE <25071a12e2@vnrvjiet.in>
2026-08-17 07:22:14 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (23.70%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -9,25 +9,9 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        # n=0
-        # head1 = head
-        # while head1!=None:
-        #     n+=1
-        #     head1 = head1.next
-        # if n% 2 == 0:
-        #     n = n//2
-        #     n = n
-        # else:
-        #     n = n//2
-        # temp = head
-        # while n>0:
-        #     temp = temp.next
-        #     n-=1
-
-        # return temp
         slow = head
         fast = head
-        while fast!=None and fast.next!=None:
+        while fast and fast.next:
             slow =  slow.next
             fast = fast.next.next
 
