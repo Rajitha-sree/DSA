8e737ea5e07e6672bd38f070067f2065eabeea5a
BODDU RAJITHA SREE <25071a12e2@vnrvjiet.in>
2026-08-17 06:58:43 +0530

    Time: 6 ms (61.88%) | Memory: 13.6 MB (19.78%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        j =0
+        j = 0
         for i in range(len(nums)):
             if nums[i]==0:
                 continue
