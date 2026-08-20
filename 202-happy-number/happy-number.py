0dc8b48ac6427aaf5f1a79ff5f0e23025f7e020c
BODDU RAJITHA SREE <25071a12e2@vnrvjiet.in>
2026-08-20 21:57:18 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (18.11%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -11,17 +11,33 @@ class Solution(object):
                 n = n//10
                 sum+=rem*rem
             return sum
-        seen = set()
-        if n==1:
-            return True
-        while n!=1:
-            n = num(n)
-            if n in seen:
+        # seen = set()
+        # if n==1:
+        #     return True
+        # while n!=1:
+        #     n = num(n)
+        #     if n in seen:
+        #         return False
+        #     else:
+        #         if n == 1:
+        #             return True
+        #         else:
+        #             seen.add(n)
+        slow = n
+        fast = n
+        slow = num(slow)
+        fast = num(num(fast))
+
+        while fast!=1:
+            slow = num(slow)
+            fast = num(num(fast))
+            if fast == slow:
                 return False
-            else:
-                if n == 1:
-                    return True
-                else:
-                    seen.add(n)
+            
+        if fast == 1:
+            return True
+        return False
+        
+        
 
                 
