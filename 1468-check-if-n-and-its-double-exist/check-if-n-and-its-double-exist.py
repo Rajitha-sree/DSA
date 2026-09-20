f275d0ab50690638cbef48d599733511afd5a11a
BODDU RAJITHA SREE <25071a12e2@vnrvjiet.in>
2026-09-20 21:42:53 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (44.94%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
@@ -4,12 +4,22 @@ class Solution(object):
         :type arr: List[int]
         :rtype: bool
         """
-        n = len(arr)
-        for i in range(n):
-            for j in range(n):
-                if(i!=j):
-                    if(arr[i] == 2*arr[j]):
-                        return True
+        #Brute-force-approach
+        # n = len(arr)
+        # for i in range(n):
+        #     for j in range(i+1,n):
+        #         if(i!=j):
+        #             if(arr[i] == 2*arr[j] or arr[j] == 2*arr[i]):
+        #                 return True
+
+        # return False
+
+        #hash set
+        seen = set()
+        for i in arr:
+            if(2*i in seen) or (i%2==0 and i//2 in seen):
+                return True
+            seen.add(i)
 
         return False
         
