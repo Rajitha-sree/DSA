9a5a9b54cf54d75ae3c2720341f31179469bf5e2
BODDU RAJITHA SREE <25071a12e2@vnrvjiet.in>
2026-09-20 21:34:52 +0530

    Time: 15 ms (29.53%) | Memory: 12.4 MB (44.94%) - LeetSync

---
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def checkIfExist(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        n = len(arr)
+        for i in range(n):
+            for j in range(n):
+                if(i!=j):
+                    if(arr[i] == 2*arr[j]):
+                        return True
+
+        return False
+        
