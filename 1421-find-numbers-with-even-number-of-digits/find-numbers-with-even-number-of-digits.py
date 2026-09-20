c13c6b35f47a1cf95772693ae5727e7f5c0acba3
BODDU RAJITHA SREE <25071a12e2@vnrvjiet.in>
2026-09-20 21:31:57 +0530

    Time: 4 ms (32.60%) | Memory: 12.3 MB (73.20%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.py
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.py
@@ -1,17 +1,15 @@
 class Solution(object):
     def findNumbers(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        n = len(nums)
         count = 0
-        for i in nums:
+
+        for num in nums:
             c = 0
-            while(i>0):
-                c+=1
-                i=i//10
-            if(c%2==0):
-                count+=1
-        
+
+            while num > 0:
+                c += 1
+                num //= 10
+
+            if c % 2 == 0:
+                count += 1
+
         return count
